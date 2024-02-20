3f23348a3139d0d85711b4c6c4a66851b99659b7
Zach Servies <zachservies@gmail.com>
2024-02-19 21:21:11 -0500

    Added window buttons.

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -3,7 +3,14 @@ import './Landing.css';
 export default function Landing() {
   return (
     <div className="landing-wrapper">
-      <h1>Name</h1>
+      <div className="window-buttons">
+        <button className="dot-exit"></button>
+        <button className="dot-min"></button>
+        <button className="dot-max"></button>
+      </div>
+      <div className="landing-window">
+        <h1>Name</h1>
+      </div>
     </div>
   )
 }
