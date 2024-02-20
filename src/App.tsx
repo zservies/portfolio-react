8a645dc806bf88b17e2e19b37f52551cf0caed60
Zach Servies <zachservies@gmail.com>
2024-02-19 21:50:44 -0500

    Added i18next for translations and text constants.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import Landing from './components/Landing';
 import './App.css'
 
--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,6 +1,8 @@
 import './Landing.css';
+import { useTranslation } from 'react-i18next';
 
 export default function Landing() {
+  const { t } = useTranslation();
   return (
     <div className="landing-wrapper">
       <div className="window-buttons">
@@ -9,7 +11,7 @@ export default function Landing() {
         <button className="dot-max"></button>
       </div>
       <div className="landing-window">
-        <h1>Name</h1>
+        <h1>{t('landing.name')}</h1>
       </div>
     </div>
   )
