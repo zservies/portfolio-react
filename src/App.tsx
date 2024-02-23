6a57498810681e31780fc9dd95e6d83b6ef052db
Zach Servies <zachservies@gmail.com>
2024-02-22 23:58:29 -0500

    Created toolbar component. Styling updates.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import Landing from './components/Landing';
+import ToolbarHeader from './components/ToolbarHeader';
 import './App.css'
 
 function App() {
 
   return (
     <div className="app-wrapper">
+      <ToolbarHeader></ToolbarHeader>
       <Landing></Landing>
+      <div className="empty-flex"></div>
     </div>
   )
 }
---
+++ src/components/ToolbarHeader.tsx
@@ -0,0 +1,9 @@
+import './ToolbarHeader.css';
+
+export default function ToolbarHeader() {
+  return (
+    <div className="toolbar-header-wrapper">
+      Toolbar
+    </div>
+  )
+}
